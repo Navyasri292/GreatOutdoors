de42a5cced072de1ca60e8fd6c647d0f88e4a98b
BVRIT <BVRIT@EEE-NEWLAB-223>
2020-02-20 19:59:47 +0530

    success

---
+++ src/main/java/com/cg/go/bean/User.java
@@ -0,0 +1,53 @@
+package com.cg.go.bean;
+
+public class User
+{
+private String userName;
+private String passWord;
+private String email;
+private String phoneNum;
+private boolean status;
+public User(String userName, String passWord, String email, String phoneNum) {
+	super();
+	this.userName = userName;
+	this.passWord = passWord;
+	this.email = email;
+	this.phoneNum = phoneNum;
+}
+public User() {
+	super();
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getPassWord() {
+	return passWord;
+}
+public void setPassWord(String passWord) {
+	this.passWord = passWord;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPhoneNum() {
+	return phoneNum;
+}
+public void setPhoneNum(String phoneNum) {
+	this.phoneNum = phoneNum;
+}
+public boolean isStatus() {
+	return status;
+}
+public void setStatus(boolean status) {
+	this.status = status;
+}
+
+
+
+}
